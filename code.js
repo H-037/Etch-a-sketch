f7bc43b797708c001a04d9368a18b8e564327cb9
H-037 <122795604+H-037@users.noreply.github.com>
2023-02-20 18:54:36 +0100

    add foundation

---
+++ code.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("container");
+const box = document.createElement("div");
+box.classList.add("box");
+
+container.appendChild(box);
+container.appendChild(box);
