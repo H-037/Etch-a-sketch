119c7b2df574389c528b43fe3aeeb1e41389ef75
H-037 <122795604+H-037@users.noreply.github.com>
2023-02-20 20:09:14 +0100

    add algorithm

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 const container = document.getElementById("container");
-const box = document.createElement("div");
-box.classList.add("box");
 
-container.appendChild(box);
-container.appendChild(box);
+for (i=0; i<256; i++){
+    const box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+    box.addEventListener("mouseover", changecolor);
+    function changecolor(){
+        box.style.backgroundColor = "white";
+    }
+}
