6d3a7014735afc311329be822ff34fc0a88d5bfd
H-037 <122795604+H-037@users.noreply.github.com>
2023-02-20 20:39:54 +0100

    adding box css to js

--- code.js
+++ code.js
@@ -2,13 +2,25 @@ const container = document.getElementById("container");
 const button = document.getElementById("reset");
 
 for (i=0; i<256; i++){
+    //adding boxes
     const box = document.createElement("div");
     box.classList.add("box");
     container.appendChild(box);
+
+    //adding box css into js
+    let f = 6.25;
+    let x = f+"%";
+    box.style.height = x;
+    box.style.width = x;
+    box.style.backgroundColor = "blue";
+
+    //adding drawing 
     box.addEventListener("mouseover", changecolor);
     function changecolor(){
         box.style.backgroundColor = "black";
     }
+
+    //adding reset
     button.addEventListener("click", reset);
     function reset(){
         box.style.backgroundColor = "white";
