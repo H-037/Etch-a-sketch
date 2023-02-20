7f23b7acfa8fc31af2899f0c75c6e53d0a55149a
H-037 <122795604+H-037@users.noreply.github.com>
2023-02-20 21:32:08 +0100

    adding ratio scale/ new game

--- code.js
+++ code.js
@@ -1,18 +1,24 @@
 const container = document.getElementById("container");
-const button = document.getElementById("reset");
+const resetbutton = document.getElementById("reset");
+const ratiobutton = document.getElementById("ratio");
 
-for (i=0; i<256; i++){
+
+let f = prompt("Type in ratio number under 100");
+let x = 100/f;
+let w = f*f;
+
+for (i=0; i<w; i++){
     //adding boxes
     const box = document.createElement("div");
     box.classList.add("box");
     container.appendChild(box);
 
     //adding box css into js
-    let f = 6.25;
-    let x = f+"%";
-    box.style.height = x;
-    box.style.width = x;
-    box.style.backgroundColor = "blue";
+    let n = x+"%";
+    console.log(n);
+    box.style.height = n;
+    box.style.width = n;
+    box.style.backgroundColor = "#FFFDE4";
 
     //adding drawing 
     box.addEventListener("mouseover", changecolor);
@@ -21,8 +27,44 @@ for (i=0; i<256; i++){
     }
 
     //adding reset
-    button.addEventListener("click", reset);
+    resetbutton.addEventListener("click", reset);
     function reset(){
         box.style.backgroundColor = "white";
+    }
+}
+
+// adding ratio/new game button
+ratiobutton.addEventListener("click", setratio);
+function setratio(){
+    let f = prompt("Type in ratio number under 100");
+    let x = 100/f;
+    let w = f*f;
+
+    container.replaceChildren();
+
+    for (i=0; i<w; i++){
+        //adding boxes
+        const box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+    
+        //adding box css into js
+        let n = x+"%";
+        console.log(n);
+        box.style.height = n;
+        box.style.width = n;
+        box.style.backgroundColor = "#FFFDE4";
+    
+        //adding drawing 
+        box.addEventListener("mouseover", changecolor);
+        function changecolor(){
+            box.style.backgroundColor = "black";
+        }
+    
+        //adding reset
+        resetbutton.addEventListener("click", reset);
+        function reset(){
+            box.style.backgroundColor = "white";
+        }
     }
 }
